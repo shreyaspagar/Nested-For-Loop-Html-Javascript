5a88f63a2e872c79723bb2e2392eff5aee90903d
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-29 19:28:24 +0530

    Add files via upload

---
+++ app42.js
@@ -0,0 +1,9 @@
+let heroes = [["superman", "batman", "wonder woman"], ["spiderman", "ironman", "thor"]];
+
+for(list of heroes) {
+    for(heroes of list) {
+        console.log(heroes);
+    }
+}
+
+
